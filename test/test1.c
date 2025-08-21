ccc8133e0fe6224456f0e9827759d02d5a2dc971
Your Name <ksoh0907@gmail.com>
2025-08-21 10:14:24 +0530

    test

---
+++ test/test1.c
@@ -0,0 +1,50 @@
+// convert the time entered in hh,mm and sec into seconds
+//void main (){
+//	int hr,min,sec,totalsec;
+//	printf("enter hr:");
+//	scanf("%d",&hr);
+//	
+//	printf("enter min:");
+//	scanf("%d",&min);
+//	
+//	printf("enter seconds :");
+//	scanf("%d",&sec);
+//	
+//	totalsec=(hr*60*60)+(min*60)+sec;
+//	printf("totalseconds are %d",totalsec);
+//	
+//}
+
+
+
+
+
+//wapp to check if the year is leap or not
+
+//void main(){
+//	int yr;
+//	printf("enter a year:");
+//	scanf("%d",&yr);
+//	if(yr%4==0&& yr%100!=0 ||yr%400==0){
+//		printf("%d is a leap year:",yr);
+//	}
+//	else{
+//			printf("%d is not a leap year:",yr);
+//	}
+//}
+
+
+
+//print last 2 digit number 33333333
+//ip 223410
+//op 10
+
+//
+//void main(){
+//	int num=223410,rem;
+////	rem=num%100;
+////	printf("remainder is %d",rem);
+//	
+//	printf("remainder is %d",num%100);
+//	
+//}
